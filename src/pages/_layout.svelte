1e0821c2936e477052bd5ec0391c80528adcf0f3
Raphael Tang <raphpb1912@gmail.com>
2021-01-10 00:18:01 +0800

    Added more cool stuff

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,8 @@
+<!-- routify:options preload="proximity" -->
+<script lang="ts">
+  import { MaterialApp } from "svelte-materialify"
+</script>
+
+<MaterialApp theme="dark">
+  <slot />
+</MaterialApp>
---
+++ src/pages/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { metatags } from "@roxi/routify"
+  import { Button, AppBar, Icon } from "svelte-materialify"
+  import { mdiMenu, mdiCloudDownloadOutline } from "@mdi/js"
+
+  metatags.title = "RetardOS"
+  metatags.description = "RetardOS Official Website"
+</script>
+
+<AppBar>
+  <div slot="icon">
+    <Button fab depressed>
+      <Icon path={mdiMenu} />
+    </Button>
+  </div>
+  <span slot="title">RetardOS</span>
+  <div style="flex-grow:1" />
+  <Button fab depressed>
+    <Icon path={mdiCloudDownloadOutline} />
+  </Button>
+</AppBar>
